f551b5b63901e567afd1c3f2e025a3cd247667f5
dekim-dev <dekimdev@gmail.com>
2023-08-01 23:52:31 +0900

    :lipstick: add DateNotice

---
+++ src/components/Home/DateNotice.js
@@ -0,0 +1,11 @@
+const DateNotice = () => {
+  let today = new Date();
+
+  return (
+    <h1>
+      📅 {today.getFullYear()}년 {today.getMonth() + 1}월 {today.getDate()}일
+    </h1>
+  );
+};
+
+export default DateNotice;
