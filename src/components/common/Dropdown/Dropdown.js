9119f68c07dc29d1415724fb76f9dd01e1b6b387
dekim-dev <dekimdev@gmail.com>
2023-08-13 02:48:04 +0900

    :lipstick: style dropdown

---
+++ src/components/common/Dropdown/Dropdown.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+import ProfileImg from "../../../assets/images/profile.jpeg";
+import NonMember from "../../../assets/images/nonMemberImg.svg";
+
+import DropdownContent from "./DropdownMenu";
+
+const DropDownWrapper = styled.div`
+  position: relative;
+`;
+
+// Keyframes 정의
+const borderAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+const ProfileIcon = styled.img`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  border: 3px solid #ececec;
+  cursor: pointer;
+  transition: transform 0.3s; /* 애니메이션을 부드럽게 하기 위한 설정 */
+  &:hover {
+    animation: zoomOut 0.5s ease-in-out; /* 애니메이션을 적용 */
+
+    @keyframes zoomOut {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(1.1);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }
+  }
+`;
+const DropDown = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+  const ATK = localStorage.getItem("ACCESS_TOKEN");
+  const [dropDownView, setDropDownView] = useState(false);
+  console.log(ATK);
+
+  useEffect(() => {
+    if (ATK === null || ATK === undefined) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+
+    console.log("isLoggedIn 렌더링");
+  }, [ATK]);
+
+  return (
+    <DropDownWrapper>
+      <div>
+        {isLoggedIn ? (
+          <ProfileIcon
+            src={ProfileImg}
+            alt="userImg"
+            onClick={() => setDropDownView(!dropDownView)}
+          />
+        ) : (
+          <ProfileIcon
+            src={NonMember}
+            alt="nonUserImg"
+            onClick={() => setDropDownView(!dropDownView)}
+          />
+        )}
+      </div>
+      {dropDownView && (
+        <DropdownContent
+          isLoggedIn={isLoggedIn}
+          setDropDownView={() => setDropDownView(!dropDownView)}
+        />
+      )}
+    </DropDownWrapper>
+  );
+};
+export default DropDown;
---
+++ src/components/common/Dropdown/DropdownMenu.js
@@ -0,0 +1,85 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const DropdownMenu = styled.ul`
+  position: absolute;
+  border: 1px solid #ececec;
+  border-radius: 8px;
+  right: 0.4rem;
+  top: 3.2rem;
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  animation: fadeIn 0.3s ease-in-out; /* 애니메이션 추가 */
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1); /* 그림자 추가 */
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(-10px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+const StyledLink = styled(Link)`
+  margin: 0;
+  padding: 1rem;
+  font-size: 1rem;
+  border-bottom: solid 1px #ececec; // 하단(중간) 선
+  text-align: center;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  &:first-child {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+
+  &:last-child {
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    border-bottom: none; /* 마지막 링크의 하단 경계선 제거 */
+  }
+
+  &:active {
+    font-weight: bold;
+  }
+`;
+
+const DropdownContent = ({ isLoggedIn, setDropDownView }) => {
+  const handleLinkClick = () => {
+    setDropDownView(false);
+  };
+
+  if (isLoggedIn) {
+    return (
+      <DropdownMenu>
+        <StyledLink to="/mypage" onClick={handleLinkClick}>
+          마이페이지
+        </StyledLink>
+        <StyledLink to="/" onClick={handleLinkClick}>
+          로그아웃
+        </StyledLink>
+      </DropdownMenu>
+    );
+  } else {
+    return (
+      <DropdownMenu>
+        <StyledLink to="/signin" onClick={handleLinkClick}>
+          로그인
+        </StyledLink>
+        <StyledLink to="/signup" onClick={handleLinkClick}>
+          회원가입
+        </StyledLink>
+      </DropdownMenu>
+    );
+  }
+};
+export default DropdownContent;
