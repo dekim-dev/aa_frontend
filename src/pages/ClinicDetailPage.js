3c646cdad38857175934a9ac75813b9977fe9800
dekim-dev <dekimdev@gmail.com>
2023-08-21 19:33:35 +0900

    :lipstick: Implement 'ClinicInfo' component
    implemented 'ClinicInfo' and 'OpenHours' components to detail page

--- src/pages/ClinicDetailPage.js
+++ src/pages/ClinicDetailPage.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
+import styled from "styled-components";
+import ClinicInfo from "../components/Clinic/Detail/ClinicInfo";
 import KakaoMap from "../components/Clinic/Detail/KakaoMap";
 
+const ParentWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  section {
+    width: 70%;
+    .section_title {
+      /* font-size: 1.5rem; */
+      line-height: 2.5rem;
+    }
+  }
+`;
+
 const ClinicDetailPage = () => {
+  const [kakaoMapInfo, setKakaoMapInfo] = useState({}); // KakaoMap 에게 줄 정보
+
   return (
-    <>
-      <KakaoMap latitude={"33.450701"} longitude={"126.570667"} name={"이름"} />
-    </>
+    <ParentWrapper>
+      <section>
+        <h2 className="section_title">📌 병원 정보</h2>
+        <ClinicInfo setKakaoMapInfo={setKakaoMapInfo} />
+      </section>
+      <section>
+        <h2 className="section_title">📍 지도에서 보기</h2>
+        <KakaoMap {...kakaoMapInfo} />
+      </section>
+    </ParentWrapper>
   );
 };
 export default ClinicDetailPage;
