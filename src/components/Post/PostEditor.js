e53a98f1649fa1dbee6529ae396ae6c80d668d7c
dekim-dev <dekimdev@gmail.com>
2023-08-31 14:56:41 +0900

    :construction: Work in progress

--- src/components/Post/PostEditor.js
+++ src/components/Post/PostEditor.js
@@ -1,27 +1,56 @@
 import { useState } from "react";
 import { styled } from "styled-components";
-import InputField from "../common/TextField";
 import { createPost } from "../../service/ApiService";
+import { FreeBoardTopics, QnABoardTopics } from "./TopicSelectBox";
+import { useNavigate } from "react-router-dom";
 
 const ParentWrapper = styled.div`
+  margin: 0 auto;
   width: 80%;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 1rem;
 `;
-const StyledTextField = styled.input``;
+const StyledTextField = styled.input`
+  border-radius: 6px;
+`;
 const StyledTextArea = styled.textarea`
   width: 300px;
   height: 500px;
+  border-radius: 10px;
 `;
 const StyledButton = styled.button``;
 
+const BoardSelectBox = ({ onChange }) => {
+  return (
+    <>
+      <select onChange={onChange}>
+        <option defaultChecked>게시판 선택</option>
+        <option value={"free"}>자유게시판</option>
+        <option value={"qna"}>질문게시판</option>
+      </select>
+    </>
+  );
+};
+
 const PostEditor = () => {
+  const navigate = useNavigate();
+  const [selectedBoard, setSelectedBoard] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState("");
+
   const [state, setState] = useState({
     title: "",
     content: "",
   });
+
+  const handleBoardChange = (e) => {
+    setSelectedBoard(e.target.value);
+  };
+
+  const handleTopicChange = (e) => {
+    setSelectedTopic(e.target.value);
+  };
 
   const handleChangeState = (e) => {
     setState({
@@ -30,30 +59,42 @@ const PostEditor = () => {
     });
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const requestData = {
+      boardCategory: selectedBoard,
+      topic: selectedTopic,
       title: state.title,
       content: state.content,
     };
 
-    createPost(requestData)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error("Error posting:", error);
-      });
+    try {
+      const response = await createPost(requestData);
+      console.log(response);
+      console.log(response.topic);
+      navigate(`/post/${response.id}`);
+    } catch (error) {
+      console.error("글 등록 에러", error);
+    }
   };
   return (
     <ParentWrapper>
-      <h1>글쓰기 페이지</h1>
-      <InputField
+      <div className="select_boxes">
+        <BoardSelectBox onChange={handleBoardChange} />
+        {selectedBoard === "free" && (
+          <FreeBoardTopics onChange={handleTopicChange} />
+        )}
+        {selectedBoard === "qna" && (
+          <QnABoardTopics onChange={handleTopicChange} />
+        )}
+      </div>
+      <StyledTextField
         label="글 제목"
         type="text"
         name="title"
         value={state.title}
         onChange={handleChangeState}
-        width={"250px"}
+        width="250px"
+        placeholder="글 제목을 입력하세요"
       />
       <StyledTextArea
         name="content"
--- src/components/Post/TopicSelectBox.js
+++ src/components/Post/TopicSelectBox.js
@@ -1,26 +1,30 @@
-const FreeBoardTopics = () => {
+const FreeBoardTopics = ({ onChange }) => {
   return (
     <>
-      <select>
-        <option>All</option>
-        <option>경험</option>
-        <option>일상</option>
-        <option>생각</option>
-        <option>진료</option>
+      <select onChange={onChange}>
+        <option defaultChecked value={""}>
+          말머리 선택
+        </option>
+        <option value="experience">경험</option>
+        <option value="daily">일상</option>
+        <option value="thoughts">생각</option>
+        <option value="treatment">진료</option>
       </select>
     </>
   );
 };
 
-const QnABoardTopics = () => {
+const QnABoardTopics = ({ onChange }) => {
   return (
     <>
-      <select>
-        <option>All</option>
-        <option>ADHD</option>
-        <option>공부</option>
-        <option>직장</option>
-        <option>etc.</option>
+      <select onChange={onChange}>
+        <option defaultChecked value={""}>
+          말머리 선택
+        </option>
+        <option value="ADHD">ADHD</option>
+        <option value="study">공부</option>
+        <option value="work">직장</option>
+        <option value="etc">etc.</option>
       </select>
     </>
   );
