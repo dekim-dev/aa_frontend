d29d13404a7d7bced99445b9e552cb3042b7c4ce
dekim-dev <dekimdev@gmail.com>
2023-11-24 23:59:57 +0900

    :construction: working on inquiry button

---
+++ src/components/Home/InquiryBtn.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button``;
+
+const InquiryBtn = () => {
+  return (
+    <>
+      <StyledButton>문의하기</StyledButton>
+    </>
+  );
+};
+export default InquiryBtn;
